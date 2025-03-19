ce4f962d8cf9cf9c681a9e423c8080f309d6dc77
User <jsrojasbuitrago@gmail.com>
2025-03-19 17:32:34 -0500

    Updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,17 +11,17 @@ const Register = () => {
       <h1 className="mb-3">🛒 Registro de Vendedores</h1>
       <p>Regístrate para vender en nuestro Marketplace</p>
 
-      {/* Botón para abrir el modal de registro */}
+      {}
       <button className="btn btn-primary me-2" onClick={() => setModalOpen(true)}>
         ✍️ Crear Cuenta
       </button>
 
-      {/* Botón para volver al inicio */}
+      {}
       <button className="btn btn-outline-secondary" onClick={() => navigate("/login")}>
         ⬅️ Volver
       </button>
 
-      {/* Modal de registro */}
+      {}
       <RegistroVendedor isOpen={modalOpen} onClose={() => setModalOpen(false)} />
     </div>
   );
